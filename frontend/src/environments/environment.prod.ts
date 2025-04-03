440777e9d93d64e6cd8405bcb4a18c32e5e97724
harishhari130615 <harish130615@gmail.com>
2025-04-03 19:23:21 +0530

    e

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
   export const environment = {
     production: true,
-    apiUrl: 'https://devf.eventdesk.io',
+    apiUrl: 'https://uatb.eventdesk.io',
     stripePublishableKey: 'pk_test_51R8XLWLx8lJLLrhoDcBwO0yCv9AulJBu2FEbnCGLBn5TB8tadChX5uRIl96X9ITOKbAwEcxvdpMYo5kAxl3uLZeV00AFVvb61v'
   };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000',
+    apiUrl: 'https://uatb.eventdesk.io',
     stripePublishableKey: 'pk_test_51R8XLWLx8lJLLrhoDcBwO0yCv9AulJBu2FEbnCGLBn5TB8tadChX5uRIl96X9ITOKbAwEcxvdpMYo5kAxl3uLZeV00AFVvb61v'
   };
   
